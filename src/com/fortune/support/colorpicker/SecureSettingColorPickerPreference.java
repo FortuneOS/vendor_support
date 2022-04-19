4face0fd4488ed75d06ad7dd991bcd553e1b27a2
afterallafk <shivamatiet2001@gmail.com>
2022-04-19 20:44:26 +0000

    support: Import ColorPreference Settings from crdroid Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: Renayura <renayura@proton.me>
    Change-Id: I02953028943eb436f61a907a70b58c3b0a760344

---
+++ src/com/fortune/support/colorpicker/SecureSettingColorPickerPreference.java
@@ -0,0 +1,26 @@
+package com.fortune.support.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.fortune.support.preferences.SecureSettingsStore;
+import com.fortune.support.colorpicker.ColorPickerPreference;
+public class SecureSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}
